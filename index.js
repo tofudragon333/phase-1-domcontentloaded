b3b9de4c69e45da406112c1c805fa3b8a0c1aaee
Ashley Wong <tofudragon333@gmail.com>
2022-05-22 19:56:48 -0400

    commit tree 2810e5620e930431566a9697b73154b2c1261d3c

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let variable = document.getElementById("text")
+    variable.textContent = "This is really cool!";
+})
